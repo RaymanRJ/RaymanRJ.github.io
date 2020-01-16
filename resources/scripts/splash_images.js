a6beba27525cb05d8d868b2d205406beecb62884
Rayman Jamal <34038866+RaymanRJ@users.noreply.github.com>
2020-01-15 19:54:23 -0500

    Create splash_images.js

---
+++ resources/scripts/splash_images.js
@@ -0,0 +1,43 @@
+// Because this is all running locally, this cannot be saved as a JSON file as it will cause CORS errors.
+// Instead, saving as a variable array of JSON objects, to be imported and used in the main script.
+
+var splash_images = [
+    {
+      "image_name": "Seoul Olympic Park",
+      "image_filename": "seoul-olympic-park.jpg",
+      "image_href": "http://english.visitkorea.or.kr/enu/ATR/SI_EN_3_1_1_1.jsp?cid=789703"
+    },
+    {
+      "image_name": "Shinjuku Goyen Park",
+      "image_filename": "shinjuku-goyen.jpg",
+      "image_href": "https://www.japan-guide.com/e/e3034_001.html"
+    },
+    {
+      "image_name": "Tiger Temple",
+      "image_filename": "tiger-temple.jpg"
+    },
+    {
+      "image_name": "Dongdaemun Design Plaza",
+      "image_filename": "dongdaemun-design-plaza-leds.jpg",
+      "image_href": "http://english.visitkorea.or.kr/enu/ATR/SI_EN_3_1_1_1.jsp?cid=2475097"
+    },
+    {
+      "image_name": "Shibuya Crossing",
+      "image_filename": "tian-tan-buddha.jpg",
+      "image_href": "https://www.lonelyplanet.com/japan/tokyo/attractions/shibuya-crossing/a/poi-sig/396831/356817"
+    },
+    {
+      "image_name": "Tian Tan Buddha",
+      "image_filename": "shibuya-crossing.jpg",
+      "image_href": "https://en.wikipedia.org/wiki/Tian_Tan_Buddha"
+    },
+    {
+      "image_name": "Tiger Temple",
+      "image_filename": "tiger-temple-2.jpg"
+    },
+    {
+      "image_name": "Waujeongsa Temple",
+      "image_filename": "waujeongsa-temple.jpg",
+      "image_href": "http://english.visitkorea.or.kr/enu/ATR/SI_EN_3_1_1_1.jsp?cid=1312088"
+  }
+]
